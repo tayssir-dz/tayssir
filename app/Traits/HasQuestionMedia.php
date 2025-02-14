84d39220e7160679cd5f1544361efb459bb92991
Mouayed Keziz <m_keziz@estin.dz>
2025-02-14 17:29:52 +0100

    -

--- app/Traits/HasQuestionMedia.php
+++ app/Traits/HasQuestionMedia.php
@@ -6,16 +6,16 @@ trait HasQuestionMedia
 {
     public function getImageAttribute()
     {
-        return $this->getFirstMediaUrl('image');
+        return $this->getFirstMediaUrl('image') ? $this->getFirstMediaUrl('image') : null;
     }
 
     public function getExplanationAssetAttribute()
     {
-        return $this->getFirstMediaUrl('explanation_asset');
+        return $this->getFirstMediaUrl('explanation_asset') ? $this->getFirstMediaUrl('explanation_asset') : null;
     }
 
     public function getHintImageAttribute()
     {
-        return $this->getFirstMediaUrl('hint_image');
+        return $this->getFirstMediaUrl('hint_image') ? $this->getFirstMediaUrl('hint_image') : null;
     }
 }
