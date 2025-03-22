e13a18cbf182dc2d4a04e204a325177f7c248701
Mouayed Keziz <m_keziz@estin.dz>
2025-03-22 20:11:56 +0100

    -

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>TAYSSIR</title>
+    <meta name="robots" content="noindex">
+    <title>{{ env('APP_NAME') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
