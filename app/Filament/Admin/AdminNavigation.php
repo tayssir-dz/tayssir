980220720a263ef08a2deaf40d1d7c20073c2151
Mouayed Keziz <m_keziz@estin.dz>
2025-09-01 03:39:08 +0100

    -

--- app/Filament/Admin/AdminNavigation.php
+++ app/Filament/Admin/AdminNavigation.php
@@ -12,91 +12,91 @@ class AdminNavigation
     public const POINTS_GROUP = 'custom.nav.section.points';
     public const APP_GROUP = 'custom.nav.section.app';
 
-    public const array PLATFORM_SETTINGS = [
+    public const PLATFORM_SETTINGS = [
         'icon' => 'heroicon-o-globe-alt',
         'sort' => 1,
         'group' => self::PLATFORM_GROUP,
     ];
 
-    public const array APP_SETTINGS = [
+    public const APP_SETTINGS = [
         'icon' => 'heroicon-o-cog-6-tooth',
         'sort' => 2,
         'group' => self::APP_GROUP,
     ];
 
-    public const array BANNER_RESOURCE = [
+    public const BANNER_RESOURCE = [
         'icon' => 'heroicon-o-photo',
         'sort' => 3,
         'group' => self::APP_GROUP,
     ];
 
-    public const array USERS = [
+    public const USERS = [
         'icon' => 'heroicon-o-users',
         'sort' => 4,
         'group' => self::MANAGEMENT_GROUP,
     ];
 
-    public const array LEADER_BOARD_RESOURCE = [
+    public const LEADER_BOARD_RESOURCE = [
         'icon' => 'heroicon-o-numbered-list',
         'sort' => 5,
         'group' => self::MANAGEMENT_GROUP,
     ];
 
-    public const array REFERRAL_SOURCE_RESOURCE = [
+    public const REFERRAL_SOURCE_RESOURCE = [
         'icon' => 'heroicon-o-share',
         'sort' => 6,
         'group' => self::MANAGEMENT_GROUP,
     ];
 
-    public const array SUBSCRIPTION_RESOURCE = [
+    public const SUBSCRIPTION_RESOURCE = [
         'icon' => 'heroicon-o-banknotes',
         'sort' => 7,
         'group' => self::SUBSCRIPTION_AND_PAYMENT_GROUP,
     ];
 
-    public const array DISCOUNT_RESOURCE = [
+    public const DISCOUNT_RESOURCE = [
         'icon' => 'heroicon-o-percent-badge',
         'sort' => 8,
         'group' => self::SUBSCRIPTION_AND_PAYMENT_GROUP,
     ];
 
-    public const array PROMOTER_RESOURCE = [
+    public const PROMOTER_RESOURCE = [
         'icon' => 'heroicon-o-megaphone',
         'sort' => 9,
         'group' => self::SUBSCRIPTION_AND_PAYMENT_GROUP,
     ];
 
-    public const array PAYMENT_RESOURCE = [
+    public const PAYMENT_RESOURCE = [
         'icon' => 'heroicon-o-banknotes',
         'sort' => 10,
         'group' => self::SUBSCRIPTION_AND_PAYMENT_GROUP,
     ];
 
-    public const array CHAPTER_LEVEL_RESOURCE = [
+    public const CHAPTER_LEVEL_RESOURCE = [
         'icon' => 'heroicon-o-rectangle-stack',
         'sort' => 11,
         'group' => self::POINTS_GROUP,
     ];
 
-    public const array DIVISION_RESOURCE = [
+    public const DIVISION_RESOURCE = [
         'icon' => "heroicon-o-academic-cap",
         'sort' => 12,
         'group' => self::CONTENT_GROUP,
     ];
 
-    public const array MATERIAL_RESOURCE = [
+    public const MATERIAL_RESOURCE = [
         'icon' => "heroicon-o-academic-cap",
         'sort' => 13,
         'group' => self::CONTENT_GROUP,
     ];
 
-    public const array UNIT_RESOURCE = [
+    public const UNIT_RESOURCE = [
         'icon' => "heroicon-o-document-duplicate",
         'sort' => 14,
         'group' => self::CONTENT_GROUP,
     ];
 
-    public const array CHAPTER_RESOURCE = [
+    public const CHAPTER_RESOURCE = [
         'icon' => "heroicon-o-rectangle-stack",
         'sort' => 15,
         'group' => self::CONTENT_GROUP,
