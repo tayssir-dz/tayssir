45b9c933f648b26078974b9ccae57069edf897e9
Mouayed Keziz <m_keziz@estin.dz>
2025-09-23 17:14:25 +0100

    -

--- routes/console.php
+++ routes/console.php
@@ -3,18 +3,5 @@
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment('keziz mouayed');
-})->purpose('Display an inspiring quote');
-
-// command otp:clean
-
-Artisan::command('otp:clean', function () {})
-    ->name('clean otp')
-    ->hourly();
-
-// Schedule::call(function () {
-//     // dd("hello world");
-//     $this->comment("hello world");
-// })->name("delete cards")
-//     ->everySecond();
+Schedule::command('otp:clean')->daily();
+Schedule::command('sanctum:prune-expired')->daily();
