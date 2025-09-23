13d5d046fd70136999ee145229e906e379dbc799
Mouayed Keziz <m_keziz@estin.dz>
2025-09-23 16:39:36 +0100

    -

--- config/sanctum.php
+++ config/sanctum.php
@@ -49,7 +49,7 @@ return [
     // 'expiration' => 60,
     // 'expiration' => 10000,
     'expiration' => null,    // default laravel expiration set as null
-    'access_token_expiration' => 60,              // One hour
+    'access_token_expiration' => 1,              // One hour
     'refresh_token_expiration' => 30 * 24 * 60,  // 7 Days
     /*
     |--------------------------------------------------------------------------
