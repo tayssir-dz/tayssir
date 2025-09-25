f52dc197f5c66014cb55eff712038406426d72ac
Mouayed Keziz <m_keziz@estin.dz>
2025-09-25 13:20:55 +0100

    -

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+use Spatie\Health\Commands\RunHealthChecksCommand;
 
 Schedule::command('otp:clean')->daily();
 Schedule::command('sanctum:prune-expired')->daily();
 Schedule::command('media-library:clean')->daily();
 Schedule::command('models:prune')->daily();
 Schedule::command('auth:clear-resets')->daily();
+Schedule::command(RunHealthChecksCommand::class)->daily();
