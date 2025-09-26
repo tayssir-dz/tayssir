1330109abd298e5d434842af0a14df6717d5c391
Mouayed Keziz <m_keziz@estin.dz>
2025-09-26 18:40:49 +0100

    -

--- resources/views/components/filament-ui/avatar.blade.php
+++ resources/views/components/filament-ui/avatar.blade.php
@@ -2,7 +2,7 @@
 
 <div {{ $attributes->class(['filament-avatar flex items-center justify-center']) }}>
     <img
-        src="{{ $avatarUrl }}"
+        src="{{ $avatar_url }}"
         alt="{{ $name }}"
         class="w-10 h-10 rounded-full ring-2 dark:ring-gray-500 ring-gray-300"
     />
