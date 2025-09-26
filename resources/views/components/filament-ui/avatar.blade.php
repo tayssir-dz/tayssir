e6b0a53f4f136297e2f7ddfe7a03bf2fe3b59dcd
Mouayed Keziz <m_keziz@estin.dz>
2025-09-26 18:38:10 +0100

    -

--- resources/views/components/filament-ui/avatar.blade.php
+++ resources/views/components/filament-ui/avatar.blade.php
@@ -1,15 +1,5 @@
 @props(['name', 'avatar_url' => null])
 
-@php
-    $initials = collect(explode(' ', $name))
-        ->map(fn ($segment) => mb_substr($segment, 0, 1))
-        ->join('');
-
-    $avatarUrl = $avatar_url
-        ? Storage::url($avatar_url)
-        : "https://ui-avatars.com/api/?name=" . urlencode($name) . "&color=FFFFFF&background=111827";
-@endphp
-
 <div {{ $attributes->class(['filament-avatar flex items-center justify-center']) }}>
     <img
         src="{{ $avatarUrl }}"
