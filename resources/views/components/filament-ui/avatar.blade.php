2e3abd55362cc9af585b1f81080095e4f4500392
Mouayed Keziz <m_keziz@estin.dz>
2025-09-26 18:44:47 +0100

    -

--- resources/views/components/filament-ui/avatar.blade.php
+++ resources/views/components/filament-ui/avatar.blade.php
@@ -1,9 +1,20 @@
 @props(['name', 'avatar_url' => null])
 
 <div {{ $attributes->class(['filament-avatar flex items-center justify-center']) }}>
-    <img
-        src="{{ $avatar_url }}"
-        alt="{{ $name }}"
-        class="w-10 h-10 rounded-full ring-2 dark:ring-gray-500 ring-gray-300"
-    />
+    @if ($avatar_url)
+        <img
+            src="{{ $avatar_url }}"
+            alt="{{ $name }}"
+            class="w-10 h-10 rounded-full ring-2 dark:ring-gray-500 ring-gray-300"
+        />
+    @else
+        @php
+            $initial = function_exists('mb_substr')
+                ? mb_strtoupper(mb_substr((string) $name, 0, 1))
+                : strtoupper(substr((string) $name, 0, 1));
+        @endphp
+        <div class="w-10 h-10 rounded-full ring-2 dark:ring-gray-500 ring-gray-300 bg-gray-100 text-gray-700 dark:bg-gray-700 dark:text-gray-200 flex items-center justify-center">
+            <span class="font-semibold">{{ $initial }}</span>
+        </div>
+    @endif
 </div>
