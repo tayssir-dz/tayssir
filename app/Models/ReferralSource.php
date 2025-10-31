f058d5bfb81eea158a6d26f8be5d264715360fa9
Mouayed Keziz <m_keziz@estin.dz>
2025-10-31 23:00:01 +0100

    -

--- app/Models/ReferralSource.php
+++ app/Models/ReferralSource.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class ReferralSource extends Model implements HasMedia
 {
@@ -27,12 +28,21 @@ class ReferralSource extends Model implements HasMedia
             ->singleFile();
     }
 
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        if ($media && $media->collection_name === 'icon') {
+            $this->addMediaConversion('thumb')
+                ->width(width: 150)
+                ->height(150);
+        }
+    }
+
     /**
      * Accessor for icon url (svg).
      */
     public function getIconAttribute()
     {
-        return $this->getFirstMediaUrl('icon') ?: null;
+        return $this->getFirstMediaUrl('icon', 'thumb') ?: null;
     }
 
     /**
